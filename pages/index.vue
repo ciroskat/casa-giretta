a6dd8609656a278d5c1926af067169883b342733
Ciro <scatigna89@gmail.com>
2020-08-21 14:09:12 +0200

    Bed and Breakfast

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div>
       <Logo />
       <h1 class="title">
-        Benvenuto in Giretta B&B
+        Benvenuto in Giretta Bed and Breaksfast
       </h1>
       <div class="links">
         <a
